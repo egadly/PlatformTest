ae02ad1d6e2f63b3234262d9f7f72336e5b50e4f
kmazas <kmazas.528@gmail.com>
2016-10-13 16:01:04 -0700

    Options prefab

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public Canvas options;
+    public Button volumeUp;
+    public Button volumeDown;
+    public Button controls;
+    public Button exit;
+	// Use this for initialization
+	void Start () {
+        options = options.GetComponent<Canvas>();
+        options.enabled = false;
+        volumeUp = volumeUp.GetComponent<Button>();
+        volumeDown = volumeDown.GetComponent<Button>();
+        controls = controls.GetComponent<Button>();
+        exit = exit.GetComponent<Button>();
+    }
+	
+    public void volumeUpPress()
+    {
+
+    }
+
+    public void volumeDownPress()
+    {
+
+    }
+
+    public void controlPress()
+    {
+
+    }
+
+    public void exitPress()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
