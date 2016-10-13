d8cd1fc91b35997ec9f6dc24be3d2a4f69646335
kmazas <kmazas.528@gmail.com>
2016-10-13 12:23:16 -0700

    Menu Scene Created
    Not final, just a test of how a menu could work

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText; //Play Button
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false; //Hides quit menu in start up
+    }
+	
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    { // No Button is pressed
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    { //Play button is pressed
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    { //Quit the game
+        Application.Quit();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
